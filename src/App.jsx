b8a7024b6a44d8fc129b0e8a25c8fa8a7cd60b7d
23carlosg <96149440+23carlosg@users.noreply.github.com>
2025-10-10 21:32:27 -0300

    Borrado ProfilePage

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Layout/Sidebar';
 import HomePage from './pages/HomePage';
 import BitacoraPage from './pages/BitacoraPage';
-import ProfilePage from './pages/ProfilePage';
 import JsonDataPage from './pages/JsonDataPage';
 import ApiDataPage from './pages/ApiDataPage';
 import './styles/main.css';
@@ -15,27 +14,6 @@ import Guillermo from './components/guillermo.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="app">
-//         <Sidebar />
-//         <main className="main-content">
-//           <Routes>
-//             <Route path="/" element={<HomePage />} />
-//             <Route path="/bitacora" element={<BitacoraPage />} />
-//             <Route path="/perfil/:id" element={<ProfilePage />} />
-//             <Route path="/json-data" element={<JsonDataPage />} />
-//             <Route path="/api-data" element={<ApiDataPage />} />
-//           </Routes>
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import CarlosPage from './pages/CarlosPage';
 import FrancoPage from './pages/FrancoPage';
