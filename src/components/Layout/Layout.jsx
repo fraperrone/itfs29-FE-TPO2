e9e44c9991b4673a8b5f779e94e75e65914380eb
Fran <fperronerey@gmail.com>
2025-10-13 12:17:10 -0300

    layout.css case sensitive

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Footer from "../footer/footer";
-import "./Layout.css";
+import "./layout.css";
 import { useLocation, useNavigate } from "react-router-dom";
 
 export const Layout = ({ children }) => {
